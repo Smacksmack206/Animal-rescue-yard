e82bd56f450ebe5a8000b440e0134f152b1a3f0c
Cedric Vallieu <cvallieu94@gmail.com>
2021-09-14 20:38:26 -0700

    added conditional prompts with validation

---
+++ somefile.js
@@ -0,0 +1,20 @@
+'use strict'
+
+alert("Welcome to the Rescue Yard!"); 
+let answer = prompt("Are you ready to make the decision of adopting a Pet today?")
+if (2 + 2 === 4) {
+  answer 
+}
+
+if(answer.toLowerCase() === 'no'){
+  alert('You might as well leave now then since you are not down to save the earth with me!')
+} else{
+  alert('I guess you are cool enough to come join')
+}
+let favColor = prompt('Guess, what is my favorite color?');
+
+if(favColor.toLowerCase() !== 'blue'){
+  alert('wrong color!');
+} else{
+  alert('Wow O_o are you want to get a discount or something?')
+}
