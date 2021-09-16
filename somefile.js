87ba9c186362c4abf42d8c3f5933053b956aeab8
Cedric Vallieu <cvallieu94@gmail.com>
2021-09-16 11:35:23 -0700

    fixed it all the way this time

--- somefile.js
+++ somefile.js
@@ -12,28 +12,29 @@ function generate()  {
 
 
 function loadExtras() {
-alert("Welcome to the Rescue Yard!"); 
-let answer = prompt("Are you ready to make the decision of adopting a Pet today?")
-if (2 + 2 === 4) {
-  answer 
+  alert("Welcome to the Rescue Yard!"); 
+  
+// let answer = 'yes'
+let answer = 'yes'
+
+do{
+  answer = prompt("Are you ready to make the decision of adopting a Pet today?");
+
+  if (answer == 'no'){
+    alert('Wrong answer');
+  } else if (answer.toLowerCase() == 'No'){
+    alert('wrong answer')
+  } else if (answer == 'not right now'){
+    alert('wrong answer')
+  } else if (answer == 'Not right now'){
+    alert('wrong answer')
+  }
+} while(answer != 'yes'){
+  alert('I guess you are cool enough to come join');
 }
 
-if(answer.toLowerCase() === 'no'){
-  alert('You might as well leave now then since you are not down to save the earth with me!')
-} else{
-  alert('I guess you are cool enough to come join')
-}
-let favColor = prompt('Guess, what is my favorite color?');
 
-if(favColor.toLowerCase() !== 'blue'){
-  alert('wrong color!');
-} else{
-  alert('Wow O_o are you want to get a discount or something?')
 }
-}
-
 let howmany = prompt('How many animals are you looking to adopt today');
 let images = document.querySelector('#images');
-if(howmany < 0){
-  images.append("<img src= 'https://image.shutterstock.com/shutterstock/photos/1826554397/display_1500/stock-photo-young-nice-looking-couple-wants-to-adopt-beautiful-dog-at-animal-shelter-1826554397.jpg' style='width: 400px; height: 350px;' />");
-}
+
